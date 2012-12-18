febdc3c504a0be096750d834ed23baf0831001fd
404 <404@example.com>
2012-12-18 08:48:27 +0800

    add methods and tests

--- lib/srt_parser.rb
+++ lib/srt_parser.rb
@@ -1,16 +1,26 @@
 def carriage_return_to_linefeed str
   str.gsub("\r", '')
 end
-def iyc_srt_to_text str
+def remove_bom str
+  str.gsub("\uFEFF", '')
+end
+def remove_id_and_timeline str
   result = []
   str.each_line do |line|
     result << line unless (line =~ /^\d\d.*\d\d:.*/ || line =~ /^\d+$/)
   end
   result.join
 end
-def iyc_remove_tags str
+def remove_tags str
   str.gsub(/<[^<]*>/, '')
 end
-def iyc_remove_empty_lines str
-  str.gsub(/^\s*$/, '')
+def remove_empty_lines str
+  str.gsub(/^\s*$\n/, '')
+end
+def srt_to_text str
+  r = remove_bom str
+  r = carriage_return_to_linefeed r
+  r = remove_tags r
+  r = remove_id_and_timeline r
+  remove_empty_lines r
 end
--- test/test_srt_parser.rb
+++ test/test_srt_parser.rb
@@ -3,10 +3,10 @@ require_relative '../lib/srt_parser.rb'
 require 'minitest/autorun'
 
 class TestSrtParser < MiniTest::Unit::TestCase
-  def test_iyc_remove_tags
-    assert_equal 'content', iyc_remove_tags('<tag>content</tag>'), 'simpel tags'
-    assert_equal 'content', iyc_remove_tags('<1><2>content</2></1>'), 'nested tags'
-    assert_equal 'content', iyc_remove_tags('<1>con<2>te<la>nt'), 'broken tags'
+  def test_remove_tags
+    assert_equal 'content', remove_tags('<tag>content</tag>'), 'simpel tags'
+    assert_equal 'content', remove_tags('<1><2>content</2></1>'), 'nested tags'
+    assert_equal 'content', remove_tags('<1>con<2>te<la>nt'), 'broken tags'
   end
   INPUT = '''
 1
@@ -29,15 +29,15 @@ Who says something like that
 INPUT2 = "1\ntext\n2\ntext 2"
 OUTPUT2 = "text\ntext 2"
 
-  def test_iyc_srt_to_text
-    assert_equal OUTPUT , iyc_srt_to_text(INPUT), 'case 1'
-    assert_equal OUTPUT2 , iyc_srt_to_text(INPUT2), 'case 2'
+  def test_remove_id_and_timeline
+    assert_equal OUTPUT , remove_id_and_timeline(INPUT), 'case 1'
+    assert_equal OUTPUT2 , remove_id_and_timeline(INPUT2), 'case 2'
   end
 
-  def test_iyc_remove_empty_lines
-    assert_equal '', iyc_remove_empty_lines("\n"), 'line feed only'
-    assert_equal '', iyc_remove_empty_lines("  \n"), 'spaces'
-    assert_equal '', iyc_remove_empty_lines("\t\n"), 'tabs'
-    assert_equal '', iyc_remove_empty_lines("\t \n"), 'spaces and tabs'
+  def test_remove_empty_lines
+    assert_equal '', remove_empty_lines("\n"), 'line feed only'
+    assert_equal '', remove_empty_lines("  \n"), 'spaces'
+    assert_equal '', remove_empty_lines("\t\n"), 'tabs'
+    assert_equal '', remove_empty_lines("\t \n"), 'spaces and tabs'
   end
 end
